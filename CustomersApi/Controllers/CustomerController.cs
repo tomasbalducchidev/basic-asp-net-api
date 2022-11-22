66ad182373c3d4bdd4f7d0450507a267f4f29f19
tomasbalducchi <tomasbalducchi@gmail.com>
2022-11-22 18:52:01 -0300

    basic crud

---
+++ CustomersApi/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using CustomersApi.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        //api/customer/id
+        [HttpGet("{id}")]
+        public async Task<CustomerDto> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //api/customer
+        [HttpGet]
+        public async Task<List<CustomerDto>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<CustomerDto> CreateCustomer(CreateCustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomersApi/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomersApi.Dtos
+{
+    public class CreateCustomerDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
